43d6719b2dfe2125ad6a210035a1b0634258e551
Bhoomika Dua <dua.bhoomika@gmail.com>
2020-09-03 18:23:45 +0530

    Semi-Final Project

--- README.md
+++ README.md
@@ -1,1 +1,145 @@
-# EventHub
+# EventHub Development Process
+A full-stack Node.js project with RESTful routing : EventHub
+
+![home-page](eventhub.bmp)
+
+## Initial Setup
+* Add Landing Page
+* Add Events Page that lists all Events
+
+Each Event has:
+   * Name
+   * Image
+
+## Layout and Basic Styling
+* Create header and footer partials
+* Add in Bootstrap
+
+## Creating New Events
+* Setup new Event POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+## Style the Events page
+* Add a better header/title
+* Make Events display in a grid
+
+## Style the Navbar and Form
+* Add a navbar to all templates
+* Style the new Event form
+
+## Add Mongoose
+* Install and configure Mongoose
+* Setup Event model
+* Use Event model inside of routes
+
+## Show Page
+* Review the RESTful routes we've seen so far
+* Add description to the Event model
+* Show db.collection.drop()
+* Add a show route/template
+
+## Refactor Mongoose Code
+* Create a models directory
+* Use module.exports
+* Require everything correctly!
+
+## Add Seeds File
+* Add a seeds.js file
+* Run the seeds file every time the server starts
+
+## Add the Comment model!
+* Make comment errors go away!
+* Display comments on Event show page
+
+## Comment New/Create
+* Discuss nested routes
+* Add the comment new and create routes
+* Add the new comment form
+
+## Style Show Page
+* Add sidebar to show page
+* Display comments nicely
+
+## Finish Styling Show Page
+* Add public directory
+* Add custom stylesheet
+
+## Authentication Pt. 1 - Add User Model
+* Install all packages needed for auth
+* Define User model 
+
+## Authentication Pt. 2 - Register
+* Configure Passport
+* Add register routes
+* Add register template
+
+## Authentication Pt. 3 - Login
+* Add login routes
+* Add login template
+
+## Authentication Pt. 4 - Logout/Navbar
+* Add logout route
+* Prevent user from adding a comment if not signed in
+* Add links to navbar
+
+## Authentication Pt. 5 - Show/Hide Links
+* Show/hide auth links in navbar 
+
+## Refactor The Routes
+* Use Express router to reoragnize all routes
+
+## Users + Comments
+* Associate users and comments
+* Save author's name to a comment automatically
+
+## Users + Events
+* Prevent an unauthenticated user from creating a Event
+* Save username+id to newly created Event
+
+## Editing Events
+* Add Method-Override
+* Add Edit Route for Events
+* Add Link to Edit Page
+* Add Update Route
+
+## Deleting Events
+* Add Destroy Route
+* Add Delete button
+
+## Authorization Part 1: Events
+* User can only edit his/her Events
+* User can only delete his/her Events
+* Hide/Show edit and delete buttons
+
+## Editing Comments
+* Add Edit route for comments
+* Add Edit button
+* Add Update route
+
+Event Edit Route: /Events/:id/edit
+Comment Edit Route:    /Events/:id/comments/:comment_id/edit
+
+## Deleting Comments
+* Add Destroy route
+* Add Delete button
+
+Event Destroy Route: /Events/:id
+Comment Destroy Route:    /Events/:id/comments/:comment_id
+
+## Authorization Part 2: Comments
+* User can only edit his/her comments
+* User can only delete his/her comments
+* Hide/Show edit and delete buttons
+* Refactor Middleware to a single file
+
+## Adding in Flash!
+* Demo working version
+* Install and configure connect-flash
+* Add bootstrap alerts to header
+
+## Adding dynamic price tag
+* Show user-defined price
+* Edit new or old price
+* Change model for Event
---
+++ middleware/index.js
@@ -0,0 +1,95 @@
+// all middlewares are here
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// declare a empty middleware object
+var middlewareObj = {};
+
+// middleware to detect if the user is logged in
+middlewareObj.isLoggedIn = function(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    // send flash message for the next request if error
+    req.flash("error", "You need to be logged in to do that!");
+    res.redirect("/login");
+}
+
+// middleware to detect errors in login form
+middlewareObj.checkLogInForm = function(req, res, next) {
+    if (req.body.username && req.body.password) {
+        return next();
+    } else {
+        // send flash message for the next request if error
+        req.flash("error", "You need to fill in both the Credentials");
+        res.redirect("/login");
+    }
+}
+
+// middleware to detect errors in register form
+middlewareObj.checkRegisterForm = function(req, res, next) {
+    if (!(req.body.username && req.body.password && req.body.repassword)) {
+        // send flash message for the next request if error
+        req.flash("error", "You need to fill all the fields of the form");
+        res.redirect("/register");
+    } else if (req.body.password != req.body.repassword) {
+        // send flash message for the next request if error
+        req.flash("error", "Re-typedPassword doesn't match, Please Try Again!");
+        res.redirect("/register");
+    } else {
+        return next();
+    }
+}
+
+// middleware to check if the user has the campground ownership
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    // is user logged in
+    if (req.isAuthenticated()) {
+        // find the campground with the requested id
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if (err) {
+                req.flash("error", "Campground not found!");
+                res.redriect("back");
+            } else {
+                // does the user own the campground?
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    req.flash("error", "You don't have permission to do that!");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        // send flash notification to user to log in first
+        req.flash("error", "You need to be logged in to do that!");
+        res.redirect("back");
+    }
+}
+
+// middleware to check if the user has the comment ownership
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+    // is user logged in
+    if (req.isAuthenticated()) {
+        // find the campground with the requested id
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redriect("back");
+            } else {
+                // does the user own the comment?
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    req.flash("error", "You don't have permission to do that!");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        // send flash notification to user to log in first
+        req.flash("error", "You don't have permission to do that!");
+        res.redirect("back");
+    }
+}
+
+module.exports = middlewareObj;
